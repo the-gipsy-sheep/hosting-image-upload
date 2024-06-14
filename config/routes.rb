31eacafbaa2cd298a7c215e1615dd561abf998e3
Demian Yovanovich <subwaydemiany@gmail.com>
2024-06-14 10:30:13 -0300

    scaffold article

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # localhost:3000
+  root "articles#index"
+  resources :articles, except: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
